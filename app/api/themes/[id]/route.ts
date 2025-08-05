e92349b2a8d30994067bdae2e32b9ca7bc82a439
diyra418@gmail.com <diyra418@gmail.com>
2025-08-05 09:27:20 +0800

    a bittt

--- app/api/themes/[id]/route.ts
+++ app/api/themes/[id]/route.ts
@@ -1,18 +1,22 @@
 import pool from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest) {
+    const url = new URL(req.url)
+    const id = url.pathname.split('/').pop()
     const { name, imageUrl, previewUrl, category } = await req.json()
     const result = await pool.query(
         'UPDATE themes SET name = $1, image_url = $2, preview_url = $3, category = $4 WHERE id = $5 RETURNING *',
-        [name, imageUrl, previewUrl, category, params.id]
+        [name, imageUrl, previewUrl, category, id]
     )
     return NextResponse.json(result.rows[0])
 }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(req: NextRequest) {
+    const url = new URL(req.url)
+    const id = url.pathname.split('/').pop()
     await pool.query(
-        'DELETE * FROM themes WHERE id = $1', [params.id]
+        'DELETE * FROM themes WHERE id = $1', [id]
     )
     return NextResponse.json({ success: true })
 }
